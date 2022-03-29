6395ddc664920753b9ab59c5abf7cdbc7a3280ea
David Veszelovszki <veszelovszki@gmail.com>
2022-03-29 12:15:23 +0200

    Add test notification with a custom icon and a form field

--- src/main/java/Copy.java
+++ src/main/java/Copy.java
@@ -1,8 +1,11 @@
 import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.NotificationType;
 import com.intellij.notification.Notifications;
 import com.intellij.openapi.ide.CopyPasteManager;
+import com.intellij.openapi.util.IconLoader;
 
+import javax.swing.*;
 import java.awt.datatransfer.StringSelection;
 
 public class Copy extends FileAction {
@@ -17,10 +20,23 @@ public class Copy extends FileAction {
         // Display bubble
         Notification notification = new Notification("Sourcegraph", "Sourcegraph",
                 "File URL copied to clipboard."+shortenURI, NotificationType.INFORMATION);
+        Notification notification2 = new Notification("Sourcegraph NPS", "NPS test",
+                "", NotificationType.INFORMATION);
+        Icon sourcegraphIcon = IconLoader.getIcon("/icons/icon.png", Copy.class);
+        notification2.setIcon(sourcegraphIcon);
+        notification2.setContent("<html>\n" +
+                                 "    <body>\n" +
+                                 "        <form accept-charset='UTF-8' action='action_page.php' autocomplete='off' method='GET' target='_blank'>\n" +
+                                 "    	    <label for='name'>Name</label><br /> \n" +
+                                 "    	    <input name='name' type='text' value='Frank' /> <br /> \n" +
+                                 "        </form>\n" +
+                                 "    </body>\n" +
+                                 "</html>");
 //        Editor.getProject
 //        NotificationGroupManager.getInstance().getNotificationGroup("Sourcegraph")
 //                .createNotification("File URL copied to clipboard."+shortenURI, NotificationType.INFORMATION)
 //                .notify(this.);
         Notifications.Bus.notify(notification);
+        Notifications.Bus.notify(notification2);
     }
 }
