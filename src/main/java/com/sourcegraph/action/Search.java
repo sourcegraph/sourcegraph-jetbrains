1b762e1d043cb9faabac56594b54c3b68f50747a
Paul Piper <pp@ilscipio.com>
2022-04-14 17:06:21 +0200

    Optimized class imports

--- src/main/java/com/sourcegraph/action/Search.java
+++ src/main/java/com/sourcegraph/action/Search.java
@@ -1,18 +1,6 @@
 package com.sourcegraph.action;
 
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.SelectionModel;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.application.ApplicationInfo;
-
-import javax.annotation.Nullable;
 
 public class Search extends SearchActionBase {
     @Override
