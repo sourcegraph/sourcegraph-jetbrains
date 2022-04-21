e8ad9f90587673216cf603d38014e71d65cfa51c
Philipp Spiess <hello@philippspiess.com>
2022-04-21 15:39:12 +0200

    Workaround

--- src/main/java/com/sourcegraph/SourcegraphSearchView.java
+++ src/main/java/com/sourcegraph/SourcegraphSearchView.java
@@ -11,8 +11,13 @@ import com.intellij.openapi.ui.Splitter;
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.ui.*;
+import com.intellij.ui.jcef.JBCefBrowser;
+import com.intellij.ui.jcef.JBCefClient;
 import com.intellij.ui.treeStructure.Tree;
 import com.intellij.util.ui.JBUI;
+import org.cef.browser.CefBrowser;
+import org.cef.handler.CefKeyboardHandler;
+import org.cef.misc.BoolRef;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -21,6 +26,8 @@ import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class SourcegraphSearchView implements Disposable {
@@ -63,11 +70,61 @@ public class SourcegraphSearchView implements Disposable {
 //        splitter.setSecondComponent(previewPanel);
 
 
+
         SourcegraphWindowService service = ServiceManager.getService(project, SourcegraphWindowService.class);
         this.window = service.window;
 
 
         topPanel.add(this.window.getComponent());
+
+        JBCefBrowser browser = this.window.getBrowser();
+        JBCefClient client = browser.getJBCefClient();
+
+        client.addKeyboardHandler(new CefKeyboardHandler() {
+            @Override
+            public boolean onPreKeyEvent(CefBrowser browser, CefKeyEvent event, BoolRef is_keyboard_shortcut) {
+                System.out.println("onPreKeyEvent");
+                System.out.println(event);
+                return true;
+            }
+
+            @Override
+            public boolean onKeyEvent(CefBrowser browser, CefKeyEvent event) {
+                System.out.println("onKeyEvent");
+                System.out.println(event);
+                if(event.windows_key_code == 8 && event.modifiers == 0) {
+                    System.out.println("SIMULATOR");
+                    browser.executeJavaScript(
+                            "(function backspace(){\n" +
+                                    "var textbox = document.activeElement;\n" +
+                            "var ss = textbox.selectionStart;\n" +
+                            "var se = textbox.selectionEnd;\n" +
+                            "var ln  = textbox.value.length;\n" +
+                            "\n" +
+                            "var textbefore = textbox.value.substring( 0, ss );    //text in front of selected text\n" +
+                            "var textselected = textbox.value.substring( ss, se ); //selected text\n" +
+                            "var textafter = textbox.value.substring( se, ln );    //text following selected text\n" +
+                            "\n" +
+                            "if(ss==se) // if no text is selected\n" +
+                            "{\n" +
+                            "textbox.value = textbox.value.substring(0, ss-1 ) + textbox.value.substring(se, ln );\n" +
+                            "textbox.focus();\n" +
+                            "textbox.selectionStart = ss-1;\n" +
+                            "textbox.selectionEnd = ss-1;\n" +
+                            "}\n" +
+                            "else // if some text is selected\n" +
+                            "{\n" +
+                            "textbox.value = textbefore + textafter ;\n" +
+                            "textbox.focus();\n" +
+                            "textbox.selectionStart = ss;\n" +
+                            "textbox.selectionEnd = ss;\n" +
+                            "}\n" +
+                            "\n" +
+                            "})();","", 0);
+                }
+                return false;
+            }
+        }, browser.getCefBrowser());
 
         panel.add(topPanel, BorderLayout.CENTER);
         tree.setRootVisible(false);
